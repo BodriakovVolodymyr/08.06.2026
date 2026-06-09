8ea973179dac7ad85119b90aa79a8a54376c0ec4
Dikel <dikel23149@gmail.com>
2026-06-09 09:42:15 +0300

    end 3,4,5 tasks

--- 08.06.2026/Program.cs
+++ 08.06.2026/Program.cs
@@ -4,13 +4,14 @@
     {
         static void Main()
         {
-            //Task1
+            Console.WriteLine("\n~~~~~~~~~~~~~Task1~~~~~~~~~~~~~~~~~");
             Console.WriteLine(Task1(2, 6));
 
-            //Task2
-            Employee empl = new Employee("Petro PP", new DateTime(2000, 11, 9), "0987654321");
+            Console.WriteLine("\n~~~~~~~~~~~~~Task2~~~~~~~~~~~~~~~~~");
+            Employee empl = new Employee("Petro PP", new DateTime(2000, 11, 9), "0987654321", "petro.pp@example.com", "Developer", "Develop software");
             empl.Print();
-            //Task3
+
+            Console.WriteLine("\n~~~~~~~~~~~~~Task3~~~~~~~~~~~~~~~~~");
             int[] arr = { 5, 9, 8, 4, 1, 3, 5, 4 };
             int choise = 0;
             Console.WriteLine("Enter sort type: 1 - min to max, 2 - max to min:");
@@ -22,6 +23,13 @@
             {
                 Console.Write(num + " ");
             }
+
+
+            Console.WriteLine("\n~~~~~~~~~~~~~Task4~~~~~~~~~~~~~~~~~");
+            City city = new City("Kyiv", "Ukraine", 2800000, 044, "Shevchenko, Holosiiv, Pechersk");
+            city.Print();
+            city.setPopulation(3000000);
+         
         }
 
         static int Task1(int x, int y)
@@ -69,19 +77,97 @@
         string name;
         DateTime birthday;
         string tel;
+        string mail;
+        string position;
+        string duties;
         public Employee(){ }
-        public Employee(string _name,DateTime birthday,string tel){
+        public Employee(string _name,DateTime birthday,string tel,string mail,string position,string duties){
             name = _name;
             this.birthday = birthday;
-            this.tel = tel;        
+            this.tel = tel;
+            this.mail = mail;
+            this.position = position;
+            this.duties = duties;
         }
         public string getName()
         {  return name; }
+        public DateTime getBirthday()
+        { return birthday; }
+        public string getTel()
+        { return tel; }
+        public string getMail()
+        { return mail; }
+        public string getPosition() 
+        { return position; }
+        public string getDuties()
+        { return duties; }
+
+
         public void setName(string _name)
         {  name = _name; }
+        public void setBirthday(DateTime _birthday)
+        { birthday = _birthday; }
+        public void setTel(string _tel)
+        { tel = _tel; }
+        public void setMail(string _mail)
+        { mail = _mail; }
+        public void setPosition(string _position)
+        { position = _position; }
+        public void setDuties(string _duties)
+        { duties = _duties; }
         public void Print()
         {
-            Console.WriteLine($"Name {name} Year of birth {birthday.Year} Month of birth {birthday.Month} Tel - {tel}");        
+            Console.WriteLine($"Name {name} Year of birth {birthday.Year} Month of birth {birthday.Month} Tel - {tel} Mail - {mail} Position - {position} Duties - {duties}");        
+        }    
+    }
+
+
+    class City
+    {
+        string name;
+        string country;
+        int population;
+        int tel_code;
+        string city_districts;
+        public City() { }
+        public City(string _name, string _country, int _population, int _tel_code, string _city_districts)
+        {
+            name = _name;
+            country = _country;
+            population = _population;
+            tel_code = _tel_code;
+            city_districts = _city_districts;
         }
+
+
+
+        public string getName()
+        { return name; }
+        public string getCountry()
+        { return country; }
+        public int getPopulation()
+        { return population; }
+        public int getTelCode()
+        { return tel_code; }
+        public string getCityDistricts()
+        { return city_districts; }
+
+
+        public void setName(string _name)
+        { name = _name; }
+        public void setCountry(string _country)
+        { country = _country; }
+        public void setPopulation(int _population)
+        { population = _population; }
+        public void setTelCode(int _tel_code)
+        { tel_code = _tel_code; }
+        public void setCityDistricts(string _city_districts)
+        { city_districts = _city_districts; }
+
+        public void Print()
+        {
+            Console.WriteLine($"City name {name} Country {country} Population {population} Tel code {tel_code} City districts {city_districts}");
+        }
+
     }
 }
